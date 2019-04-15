70a9955b76320256296cca1cd108fe94a2f77d73
OtisL <league@WTS22.attlocal.net>
2019-04-15 16:56:28 -0700

    Day 2 mod 4

--- src/extra/NastySurpise.java
+++ src/extra/NastySurpise.java
@@ -2,7 +2,11 @@ package extra;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,14 +31,35 @@ public class NastySurpise implements ActionListener {
 		trick.addActionListener(this);
 		treat.addActionListener(this);
 		trick.setText("Trick");
+		treat.setText("Treat");
+		frame.setVisible(true);
+		frame.pack();
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource() == trick) {
-			trick.add(cute);
-			treat.add(scary);
+			showPictureFromTheInternet(
+					"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/03/26125843/yorkshire-terrier-cover.jpg");
+		} else {
+			showPictureFromTheInternet(
+					"https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+		}
+	}
+
+	private void showPictureFromTheInternet(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/simon_says/SimonSays.java
+++ src/simon_says/SimonSays.java
@@ -6,19 +6,18 @@ package simon_says;
  */
 
 import java.awt.Component;
-import java.awt.Frame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import java.io.IOException;
 
 public class SimonSays extends KeyAdapter {
 
@@ -27,74 +26,71 @@ public class SimonSays extends KeyAdapter {
 	private int tries = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
+	int points = 0;
 
 	// Complete steps 1 - 7 before you test
-	// 1. Declare a JFrame variable
+
+	JFrame frame;
 
 	public void run() {
-		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
-		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
-		// key when
-		// 'Simon says' otherwise press a different key"
-
-		// 4. Call the showImage method to show an image
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		JOptionPane.showMessageDialog(null,
+				"Press the matching key when Simon Says press the key otherwise press a different one");
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
-		// 15. Make a points variable to track the score.
 
-		// 16. If the keyCode matches the imageIndex and "Simon says"
+		if (e.getKeyCode() == imageIndex && simonSays) {
+			points += 6556235;
+			speak("You are correct");
+		} else if (e.getKeyCode() != imageIndex && simonSays == false) {
+			points += 1342523;
+			speak("You are correct");
+		}
 
-		// 17. Increase the value of score
+		tries++;
 
-		// 18. Use the speak method to tell the user they were correct
+		if (tries > 2) {
+			points = 999999;
+			speak("Your score is " + points);
+			System.exit(0);
+		}
 
-		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't
-		// say..."
+		frame.dispose();
 
-		// 20. Increase the value of score
-
-		// 21. Use the speak method to tell the user they were correct
-
-		// 22. Increment tries by 1
-
-		// 25. If tries is greater than 9 (or however many you want)...
-
-		// 26. Tell the user their score
-
-		// 27. Exit the program
-
-		// 23. Dispose of the frame
-
-		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
-		// 5. Initialize your frame to a new JFrame()
 
-		// 6. Set the frame to visible
+		frame = new JFrame();
 
-		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		frame.setVisible(true);
 
-		// 8. Set the name of your frame
+		frame.add(getNextRandomImage());
 
-		// 9. Pack the frame
+		frame.pack();
 
-		// 10. Set the defaultCloseOperation of your from to
-		// JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// 11. Add a key listener to the frame
+		frame.addKeyListener(this);
 
-		// 12. Create a new instance of Random
+		Random random = new Random();
 
-		// 13. Use the Random and the speak method to either say
-		// "Simon says press this key" or "Press this key"
-
-		// 14. Above, set the value of simonSays to true/false appropriately
+		int oof = random.nextInt(2);
+		if (oof == 0) {
+			// Simon actually says
+			speak("Simon says press this key");
+			simonSays = true;
+		} else {
+			speak("Press this key");
+			simonSays = false;
+		}
 
 	}
 
